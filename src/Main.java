e9399f3a7300e2372c19abbbab1450217f92ff4f
Roman <bashkatovr@mail.ru>
2022-12-20 20:19:03 +0300

    5

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Employee[] empl = new Employee[5];
         empl[0] = new Employee("Иванов Иван Иванович", 1, 25500f);
         empl[1] = new Employee("Петров Петр Петрович", 2, 31450f);
-        empl[2] = new Employee("Сергеев Сейргей Сергеевич", 3, 40200f);
+        empl[2] = new Employee("Сергеев Сергей Сергеевич", 3, 40200f);
         empl[3] = new Employee("Романов Роман Романович", 4, 50200f);
         empl[4] = new Employee("Александров Александр Александрович", 5, 64200f);
 
