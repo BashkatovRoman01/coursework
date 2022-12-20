fcec5688b5d6ff802d3b0e3aec596d35a8c99e54
Roman <bashkatovr@mail.ru>
2022-12-20 20:09:03 +0300

    3

--- src/Employee.java
+++ src/Employee.java
@@ -46,6 +46,18 @@ public class Employee {
 
     @Override
     public String toString() {
-        return "Id: " + id + " Fio: " + FIO + " Dept: " + department + " Salary: " + Salary;
+        return "Id: " + id + " Fio: " + FIO + " dept: " + department + " Salary: " + Salary;
+    }
+    public static void main(String[] args) {
+        Employee[] empl = new Employee[5];
+        empl[0] = new Employee("Иванов Иван Иванович", 1, 25500f);
+        empl[1] = new Employee("Петров Петр Петрович", 2, 31450f);
+        empl[2] = new Employee("Сергеев Сейргей Сергеевич", 3, 40200f);
+        empl[3] = new Employee("Романов Роман Романович", 4, 50200f);
+        empl[4] = new Employee("Александров Александр Александрович", 5, 64200f);
+
+        for (Employee e: empl) {
+            System.out.println(e);
+        }
     }
 }
